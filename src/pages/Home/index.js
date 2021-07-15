1326a636765c61dda2cddcb375ee899f7c37e256
nicky_ru <nikitkaruban@gmail.com>
2021-07-15 17:06:34 +0800

    connect to db

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {observer} from "mobx-react-lite";
 import {Divider} from "@chakra-ui/layout";
 import {Jumbotron} from "../../components/Home/jumbotron";
@@ -7,7 +7,27 @@ import {WhyUs} from "../../components/Home/whyUs";
 import {Brands} from "../../components/Home/brands";
 import {Contacts} from "../../components/Home/contacts";
 
+import Amplify, {API, graphqlOperation} from 'aws-amplify';
+import {listAdvantages} from "../../graphql/queries";
+
+import awsExports from "../../aws-exports";
+Amplify.configure(awsExports);
+
 export const Home = observer(() => {
+    useEffect(() => {
+        fetchAdvantages();
+    })
+
+    const fetchAdvantages = async () => {
+        try {
+            const advantagesData = await API.graphql(graphqlOperation(listAdvantages));
+            const advantages = advantagesData.data.listAdvantages.items;
+            console.log(advantages);
+        } catch (e) {
+            console.log("Error while getting advantages: ", e);
+        }
+    }
+
     return(
         <>
             <Jumbotron/>
