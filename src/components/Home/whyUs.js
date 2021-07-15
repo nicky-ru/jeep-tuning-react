60209226ac9ced4ffa4162d5fa5e2a916116a84f
nicky_ru <nikitkaruban@gmail.com>
2021-07-15 13:46:32 +0800

    add whyUs.js

--- src/components/Home/whyUs.js
+++ src/components/Home/whyUs.js
@@ -1,1 +1,86 @@
 import React from "react";
+import {observer} from "mobx-react-lite";
+import {Container, Heading, Wrap, WrapItem, Center, Text} from "@chakra-ui/layout";
+
+export const WhyUs = observer(() => {
+    return(
+        <Container
+            my={'1rem'}
+            maxW="container.lg"
+        >
+            <Heading marginBottom={'1.2rem'}>Почему мы</Heading>
+            <Wrap spacing="30px" justify="center">
+                <WrapItem>
+                    <Center
+                        w="200px"
+                        h="100px"
+                        bg={'dark.100'}
+                        borderRadius={'md'}
+                    >
+                        <Text align={'center'}>Гарантия на все выполненные работы</Text>
+                    </Center>
+                </WrapItem>
+                <WrapItem>
+                    <Center
+                        w="200px"
+                        h="100px"
+                        bg={'dark.100'}
+                        borderRadius={'md'}
+                    >
+                        <Text align={'center'}>Гарантия на все выполненные работы</Text>
+                    </Center>
+                </WrapItem>
+                <WrapItem>
+                    <Center
+                        w="200px"
+                        h="100px"
+                        bg={'dark.100'}
+                        borderRadius={'md'}
+                    >
+                        <Text align={'center'}>Гарантия на все выполненные работы</Text>
+                    </Center>
+                </WrapItem>
+                <WrapItem>
+                    <Center
+                        w="200px"
+                        h="100px"
+                        bg={'dark.100'}
+                        borderRadius={'md'}
+                    >
+                        <Text align={'center'}>Гарантия на все выполненные работы</Text>
+                    </Center>
+                </WrapItem>
+                <WrapItem>
+                    <Center
+                        w="200px"
+                        h="100px"
+                        bg={'dark.100'}
+                        borderRadius={'md'}
+                    >
+                        <Text align={'center'}>Гарантия на все выполненные работы</Text>
+                    </Center>
+                </WrapItem>
+                <WrapItem>
+                    <Center
+                        w="200px"
+                        h="100px"
+                        bg={'dark.100'}
+                        borderRadius={'md'}
+                    >
+                        <Text align={'center'}>Гарантия на все выполненные работы</Text>
+                    </Center>
+                </WrapItem>
+                <WrapItem>
+                    <Center
+                        w="200px"
+                        h="100px"
+                        bg={'dark.100'}
+                        borderRadius={'md'}
+                    >
+                        <Text align={'center'}>Гарантия на все выполненные работы</Text>
+                    </Center>
+                </WrapItem>
+            </Wrap>
+        </Container>
+    );
+})
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import {observer} from "mobx-react-lite";
+import {Divider} from "@chakra-ui/layout";
 import {Jumbotron} from "../../components/Home/jumbotron";
 import {Services} from "../../components/Home/services";
+import {WhyUs} from "../../components/Home/whyUs";
 
 export const Home = observer(() => {
     return(
         <>
             <Jumbotron/>
             <Services/>
+            <Divider/>
+            <WhyUs/>
         </>
     )
 })
