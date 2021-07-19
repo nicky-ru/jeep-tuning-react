17eff863ea1dccf12c5cd605faa426c717266a00
nicky_ru <nikitkaruban@gmail.com>
2021-07-19 18:18:50 +0800

    hide info from footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,48 +11,49 @@ export const Footer = observer(() => {
 
     return(
         <Container py={'50px'}>
-            <Stack direction={'row'} align={'center'} spacing={2} flex={{ base: 1, md: 'auto' }} justify={'flex-end'}>
+            <Stack direction={'row'} align={'center'} spacing={2} flex={{ base: 1, md: 'auto' }} justify={'center'} mb={5}>
                 <SocialIcon url={"//api.whatsapp.com/send?phone=8615651921699"} network={'whatsapp'}/>
                 <SocialIcon url={"https://telegram.me/Vezdehod13"} network={'telegram'}/>
                 <SocialIcon url={"https://vk.com/jeepservice76"} network={'vk'}/>
                 <SocialIcon url={"https://www.instagram.com/jeepservice_76/"} network={'instagram'}/>
-                <Button
-                    leftIcon={<EmailIcon />}
-                    variant="solid"
-                    onClick={(e) => {
-                        e.preventDefault();
-                        window.location = `mailto:${email}`;
-                    }}
-                >
-                    Email
-                </Button>
-                <Button
-                    rightIcon={<PhoneIcon />}
-                    variant="outline"
-                    onClick={(e) => {
-                        e.preventDefault();
-                        window.location = `tel:${tel}`;
-                    }}
-                >
-                    Позвонить
-                </Button>
-                <Flex
-                    display={{ base: 'none', md: 'flex' }}
-                    ml={10}
-                >
-                    <Link
-                        fontSize={'1.2em'}
-                        px={'0.5rem'}
-                        textDecoration={"none"}
-                        onClick={(e) => {
-                            e.preventDefault();
-                            window.location = `tel:${tel}`;
-                        }}
-                    >
-                        {tel}
-                    </Link>
-                </Flex>
+                {/*<Button*/}
+                {/*    leftIcon={<EmailIcon />}*/}
+                {/*    variant="solid"*/}
+                {/*    onClick={(e) => {*/}
+                {/*        e.preventDefault();*/}
+                {/*        window.location = `mailto:${email}`;*/}
+                {/*    }}*/}
+                {/*>*/}
+                {/*    Email*/}
+                {/*</Button>*/}
+                {/*<Button*/}
+                {/*    rightIcon={<PhoneIcon />}*/}
+                {/*    variant="outline"*/}
+                {/*    onClick={(e) => {*/}
+                {/*        e.preventDefault();*/}
+                {/*        window.location = `tel:${tel}`;*/}
+                {/*    }}*/}
+                {/*>*/}
+                {/*    Позвонить*/}
+                {/*</Button>*/}
+                {/*<Flex*/}
+                {/*    display={{ base: 'none', md: 'flex' }}*/}
+                {/*    ml={10}*/}
+                {/*>*/}
+                {/*    <Link*/}
+                {/*        fontSize={'1.2em'}*/}
+                {/*        px={'0.5rem'}*/}
+                {/*        textDecoration={"none"}*/}
+                {/*        onClick={(e) => {*/}
+                {/*            e.preventDefault();*/}
+                {/*            window.location = `tel:${tel}`;*/}
+                {/*        }}*/}
+                {/*    >*/}
+                {/*        {tel}*/}
+                {/*    </Link>*/}
+                {/*</Flex>*/}
             </Stack>
+
             <Center>2021 Все права защищены</Center>
         </Container>
     )
