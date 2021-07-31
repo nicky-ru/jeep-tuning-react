1b013293b539ac631b7fa6721c670fc5bb54279c
nicky_ru <nikitkaruban@gmail.com>
2021-07-31 15:53:27 +0800

    add animation to brands

--- components/Brands/index.js
+++ components/Brands/index.js
@@ -1,31 +1,42 @@
 import {Container, Heading, Wrap, WrapItem, Center, Stack} from "@chakra-ui/layout";
-import {useColorModeValue} from "@chakra-ui/react";
+import {keyframes, useBreakpointValue, useColorModeValue} from "@chakra-ui/react";
 import Image from 'next/image'
 import React from "react";
 
+const slideSm = keyframes`
+    from {opacity: 10%;}
+    to {opacity: 100%;;}
+`
+
 const Brands = ({brands = []}) => {
     const bg = useColorModeValue("light.100", 'dark.100');
+    const logoSize = useBreakpointValue({base: 7, md: 9, lg: 20})
 
     return(
         <Container
             my={4}
-            maxW="container.lg"
+            maxW="full"
+            overflow={"hidden"}
         >
-            <Heading marginBottom={5}>
-                Марки, с которыми мы работаем
-            </Heading>
+            <Container maxW={"container.lg"}>
+                <Heading marginBottom={5}>
+                    Марки, с которыми мы работаем
+                </Heading>
+            </Container>
             <Wrap
                 spacing={8}
                 justify="center"
-                bg={bg}
-                p={5}
+                // bg={bg}
+                overflow={"hidden"}
+                // p={5}
                 borderRadius={'md'}
+                // position={"relative"}
             >
                 {brands.map((brand) => (
-                        <WrapItem key={brand.name}>
+                        <WrapItem key={brand.name} animation={`${slideSm} 5s infinite linear alternate`}>
                             <Center
-                                h={[6, 20]}
-                                w={[6, 20]}
+                                h={logoSize}
+                                w={logoSize}
                                 position={'relative'}
                             >
                                 <Image
