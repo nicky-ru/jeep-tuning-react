4ff18f45fbccffe89ef4d560fc72b1bb6236eb74
nicky_ru <nikitkaruban@gmail.com>
2021-08-02 15:51:28 +0800

    add tests to components

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -20,19 +20,11 @@ describe('The Home Page', () => {
     xit('should hide on small devices', function () {
         cy.get("header").contains("h1", "JeepService").should("be.visible");
         cy.get("header").contains("a", "+79108210003").should("be.visible");
-        cy.get("#services-accordion").within(() => {
-            cy.get("button:first").click()
-            cy.contains("button", "Записаться").should("be.visible");
-        })
         cy.viewport(375, 667);
         cy.get("header").contains("h1", "JeepService").should("not.be.visible");
         cy.get("header").contains("a", "+79108210003").should("not.be.visible");
-        cy.get("#services-accordion").within(() => {
-            cy.get("button:first").click()
-            cy.contains("button", "Записаться").should("not.be.visible");
-        })
     });
-})
+});
 
 describe("Jumbotron", () => {
     beforeEach(() => {
@@ -46,10 +38,81 @@ describe("Jumbotron", () => {
             cy.contains("button", "Найти услугу и записаться").should("be.visible");
         })
     });
-    xit('should be able to open a modal', function () {
+    xit('should open a modal', function () {
         cy.get("#jumbotron").within(() => {
             cy.contains("button", "Найти услугу и записаться").click();
         })
         cy.get('#chakra-modal--body-10').should("be.visible")
+    });
+});
+
+describe("Services accordion", () => {
+    beforeEach(() => {
+        cy.visit("/");
+    })
+    xit('should upload uzels', function () {
+        cy.get(".chakra-accordion").within(() => {
+            cy.contains("button", "Выпускная система").should("be.visible");
+            cy.contains("button", "Трансмиссия").should("be.visible");
+            cy.contains("button", "Ремонт рулевого управления").should("be.visible");
+            cy.contains("button", "Тормозная система").should("be.visible");
+            cy.contains("button", "Двигатель").should("be.visible");
+            cy.contains("button", "Ремонт задней подвески").should("be.visible");
+            cy.contains("button", "Ремонт передней подвески").should("be.visible");
+            cy.contains("button", "Техническое обслуживание").should("be.visible");
+        })
+    });
+    xit('should upload services', function () {
+        cy.get("#accordion-button-6").click();
+        cy.get("#services-accordion").contains("a", "Замена гофры глушителя (сварочные)")
+    });
+    xit('should open search services page', function () {
+        cy.contains("button", "Все услуги").click();
+        cy.url()
+            .should("contain", "/serviceSearch")
+    });
+    xit('should open make appointment page', function () {
+        cy.get("#accordion-button-6").click();
+        cy.contains("button", "Записаться").click();
+        cy.url({timeout: 10000})
+            .should("contain", "/appointment");
+    });
+    xit('should open service info page', function () {
+        cy.get("#accordion-button-6").click();
+        cy.contains("a", "Замена гофры глушителя (сварочные)").click();
+        cy.url({timeout: 10000})
+            .should("contain", "/service")
+    });
+    xit('should hide on small devices', function () {
+        cy.get("#services-accordion").within(() => {
+            cy.get("button:first").click()
+            cy.contains("button", "Записаться").should("be.visible");
+        })
+        cy.viewport(375, 667);
+        cy.get("#services-accordion").within(() => {
+            cy.get("button:first").click()
+            cy.contains("button", "Записаться").should("not.be.visible");
+        })
+    });
+})
+
+describe("Advantages section", () => {
+    beforeEach(() => {
+        cy.visit("/");
+    })
+    xit('should upload advantages', function () {
+        cy.contains("Вечная история Вашего авто");
+    });
+})
+
+describe("Brands section", () => {
+    beforeEach(() => {
+        cy.visit("/");
+    })
+    xit('should upload brand images', function () {
+        cy.get("#brands-section").within(() => {
+            cy.get("img:first").should("have.attr", "alt", "Suzuki");
+            cy.get("img:last").should("have.attr", "alt", "Mitsubishi");
+        })
     });
 })
