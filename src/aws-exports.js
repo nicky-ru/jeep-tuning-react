e4164a0d502b79dcd54b31522679632aa9abeca8
nicky_ru <nikitkaruban@gmail.com>
2021-08-17 13:14:55 +0800

    add amplify dev environment

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,16 +3,16 @@
 
 const awsmobile = {
     "aws_project_region": "eu-central-1",
-    "aws_appsync_graphqlEndpoint": "https://i7y2v3indbaafbcy4eesjffbee.appsync-api.eu-central-1.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://oxeshomiijg2fdbdjnaoxybxwe.appsync-api.eu-central-1.amazonaws.com/graphql",
     "aws_appsync_region": "eu-central-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-q3bzze26dnfp5guwl6rg2tcwwm",
-    "aws_cognito_identity_pool_id": "eu-central-1:c553b85a-8ad9-42b5-b77f-4cc35f1deaab",
+    "aws_appsync_apiKey": "da2-v6cfwdlphvdx3fd5lb6nkugnay",
+    "aws_cognito_identity_pool_id": "eu-central-1:07849312-fa31-4abb-881a-c83b10549be8",
     "aws_cognito_region": "eu-central-1",
-    "aws_user_pools_id": "eu-central-1_2o9jIjsSg",
-    "aws_user_pools_web_client_id": "73r1vs83b96vcv1vgudfqbuds9",
+    "aws_user_pools_id": "eu-central-1_wtUwEPASe",
+    "aws_user_pools_web_client_id": "2orjvrt6257c30snric72c874s",
     "oauth": {},
-    "aws_user_files_s3_bucket": "brandlogos152021-jt",
+    "aws_user_files_s3_bucket": "brandlogos124559-dev",
     "aws_user_files_s3_bucket_region": "eu-central-1"
 };
 
