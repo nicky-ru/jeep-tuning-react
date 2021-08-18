9fca02338afcb1aabdb9c1a95b4ee0db12a1652e
nicky_ru <nikitkaruban@gmail.com>
2021-08-18 14:30:47 +0800

    add link to home page

--- components/Shared/MdTextContainer.js
+++ components/Shared/MdTextContainer.js
@@ -1,10 +1,23 @@
-import {Container} from "@chakra-ui/layout";
+import {Container, Divider} from "@chakra-ui/layout";
 import React from "react";
 import styles from "./mdtextcontainer.module.css"
+import {Button} from "@chakra-ui/react";
+import {ArrowBackIcon} from "@chakra-ui/icons";
 
 const TextContainer = ({ policyData }) => {
     return(
         <Container mt={16} mb={4} maxW={"container.lg"}>
+            <Button
+                variant={"link"}
+                my={5}
+                leftIcon={<ArrowBackIcon/>}
+                onClick={() => {
+                    router.push("/")
+                }}
+            >
+                На главную
+            </Button>
+            <Divider my={4}/>
             <div
                 className={styles.mdtext}
                 dangerouslySetInnerHTML={{ __html: policyData.contentHtml }}
