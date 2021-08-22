bfc2ed2af8ad65e8a622134cb5be46da99d8ce3c
nicky_ru <nikitkaruban@gmail.com>
2021-08-22 15:02:58 +0800

    optimize jumbotron fonts

--- components/Jumbotron/index.js
+++ components/Jumbotron/index.js
@@ -1,5 +1,6 @@
 import {
     Heading,
+    Text,
     Center,
     Box,
     Stack,
@@ -17,11 +18,9 @@ import JumbotronModalContent from "./modalContent";
 
 const Jumbotron = ({services = []}) => {
     const bg = useColorModeValue("dark.500", "light.500");
-    const textCol = useColorModeValue("light.400", "dark.400");
     const variant = useColorModeValue("solid", "outline");
-    const modalSize = useBreakpointValue({base: "xs", md: "xl", lg: "3xl"})
+    const modalSize = useBreakpointValue({base: "sm", md: "xl", lg: "3xl"})
     const headingSize = useBreakpointValue({base: "lg", md: "xl"})
-    const secHeadingSize = useBreakpointValue({base: "xs", md: "md"})
     const jumbotronSize = useBreakpointValue({base: "xs", md: "2xl", lg: "4xl"});
     const {onOpen, onClose, isOpen} = useDisclosure();
 
@@ -52,7 +51,6 @@ const Jumbotron = ({services = []}) => {
                         maxW={jumbotronSize}
                         px={[4, 10]}
                         py={[2, 5]}
-                        // mx={[4, 0]}
                         textAlign={'center'}
                         bg={bg}
                         css={{
@@ -60,7 +58,7 @@ const Jumbotron = ({services = []}) => {
                         }}
                     >
                         <Heading size={headingSize}>Сервис внедорожников и кроссоверов c гарантией</Heading>
-                        <Heading size={secHeadingSize}>Какая услуга вас интересует?</Heading>
+                        <Text fontWeight={'bold'}>Какая услуга вас интересует?</Text>
                         <Button
                             leftIcon={<SearchIcon />}
                             onClick={onOpen}
